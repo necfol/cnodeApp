bf5e5bfe54006eae9ee173dad2c893414342e72e
necfol <necfoll@gmail.com>
2017-01-13 13:39:16 +0800

    add react-router&&add drawer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
-import { Button } from 'antd-mobile';
+import {  Drawer, List, NavBar } from 'antd-mobile';
 require('./App.css');
-
-// export default () => <h1>こんにちは世界</h1>;
-
-
 export default class App extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          open: false,
+          position: 'left'
+      };
+      this.onOpenChange = this.onOpenChange.bind(this);
+  }
+  onOpenChange(isOpen) {
+      console.log(isOpen, arguments);
+      this.setState({ open: !this.state.open });
+  }
   render() {
-    return (
-        <Button className="btn" loading>loading 按钮</Button>
-    );
+      const sidebar = (<List>
+          {[...Array(20).keys()].map((i, index) => {
+              if (index === 0) {
+                  return (<List.Item key={index}
+                                     thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+                                     multipleLine
+                  >分类</List.Item>);
+              }
+              return (<List.Item key={index}
+                                 thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+              >分类{index}</List.Item>);
+          })}
+      </List>);
+
+      const drawerProps = {
+          open: this.state.open,
+          position: this.state.position,
+          onOpenChange: this.onOpenChange,
+      };
+      return (<div style={{ height: '100%' }} onClick={this.onOpenChange}>
+        <NavBar iconName="ellipsis" ></NavBar>
+        <Drawer
+            className="my-drawer"
+            sidebar={sidebar}
+            dragHandleStyle={{ display: 'none' }}
+            contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+            {...drawerProps}
+        >
+          请点击左上角图标
+        </Drawer>
+      </div>);
   }
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router'
 import App from './components/App';
 import main from './main.css';
 ReactDOM.render(
-  <App />,
-  document.body.appendChild(document.createElement('div'))
+    (<Router history={browserHistory}>
+        <Route path="/" component={App}>
+            {/*<Route path="about" component={About}/>*/}
+            {/*<Route path="users" component={Users}>*/}
+                {/*<Route path="/user/:userId" component={User}/>*/}
+            {/*</Route>*/}
+            {/*<Route path="*" component={NoMatch}/>*/}
+        </Route>
+    </Router>),
+    document.body.appendChild(document.createElement('div'))
 );
